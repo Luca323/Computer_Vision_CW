0b0a515f577ad8264af863a5c84c83ec253f6d10
Luca323 <Luca-HDaude@hotmail.com>
2026-05-09 21:15:34 +0100

    balls

--- cv_coursework_template/finetuneKNN.m
+++ cv_coursework_template/finetuneKNN.m
@@ -1,24 +1,29 @@
 function bestParams = finetuneKNN(X, y)
-
-    kValues = [1 3 5 7 9];
+    kValues   = [1 3 5 7 9];
     distances = {'euclidean', 'minkowski', 'chebychev'};
-    
     bestParams = struct();
-    bestAcc = 0;
+    bestAcc    = 0;
     
-    cv = cvpartition(y, 'kFold', 5);
+    cv = cvpartition(y, 'KFold', 5);
     
     for k = kValues
         for d = 1:length(distances)
-            mdlCV = fitcknn(X, y, 'NumNeighbors', k, 'Distance', distances{d}, 'CVPartition', cv);
+            mdlCV = fitcknn(X, y, ...
+                'NumNeighbors', k, ...
+                'Distance',     distances{d}, ...
+                'CVPartition',  cv);
+            
             acc = 1 - kfoldLoss(mdlCV);
+            fprintf('k=%d, distance=%s, acc=%.4f\n', k, distances{d}, acc);
+            
             if acc > bestAcc
-                bestParams.acc = acc;
-                bestParams.k = k;
+                bestAcc            = acc;   % ← fix
+                bestParams.acc     = acc;
+                bestParams.k       = k;
                 bestParams.distance = distances{d};
             end
         end
     end
-
-    disp(bestParams)
+    
+    fprintf('Best: k=%d, distance=%s, acc=%.4f\n', bestParams.k, bestParams.distance, bestParams.acc);
 end
